47e46757171e9ec79e06ccff74ad371b3db5bec3
nihlet <v.andrei@protonmail.com>
2021-01-23 09:51:38 +0300

    Chart-cairo

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
 module Main where
 
-import Lib
+import Graphics.Rendering.Chart.Backend.Cairo
+import Graphics.Rendering.Chart.Easy
 
-main :: IO ()
-main = someFunc
+signal :: [Double] -> [(Double, Double)]
+signal xs = [(x, (sin (x * 3.14159 / 45) + 1) / 2 * (sin (x * 3.14159 / 5))) | x <- xs]
+
+main = do
+  print "IDDQD"
+  toFile def "example1_big.png" $ do
+    layout_title .= "Amplitude Modulation"
+    setColors [opaque blue, opaque red]
+    plot (line "am" [signal [0, (0.5) .. 400]])
+    plot (points "am points" (signal [0, 7 .. 400]))
